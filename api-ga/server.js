f6892e3250ce2b335727037e93b7bf576d902403
Rubens mesquita <Rubens mesquita>
2016-12-16 17:19:11 -0200

    Change comment

--- api-ga/server.js
+++ api-ga/server.js
@@ -30,9 +30,9 @@ let newUserGamer = new User({
     lang: 'pt-br'
 });
   
-newUserGamer.save(function(err) {
-   console.log('User added.');
-});
+// newUserGamer.save(function(err) {
+//    console.log('User added.');
+// });
 
 
 let newMissionGame =  new Missions({
